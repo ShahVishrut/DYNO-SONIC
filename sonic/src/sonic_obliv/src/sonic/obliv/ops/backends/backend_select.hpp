208eb7b818d96e7da271b79646c2269aaf453def
ShahVishrut <shahsvishrut@gmail.com>
2026-08-02 17:00:38 -0700

    Fixed heap issue

--- sonic/src/sonic_obliv/src/sonic/obliv/ops/backends/backend_select.hpp
+++ sonic/src/sonic_obliv/src/sonic/obliv/ops/backends/backend_select.hpp
@@ -15,7 +15,9 @@ namespace detail {
 using default_backend = SN_OBLIV_BACKEND_OVERRIDE;
 #else
 #if defined(__x86_64__) || defined(_M_X64) || defined(__amd64__)
-using default_backend = backends::backend_x64;
+// Force backend_cpp to avoid inline assembly syntax mismatches (AT&T vs Intel)
+// across different translation units compiled with different -masm flags.
+using default_backend = backends::backend_cpp;
 #else
 using default_backend = backends::backend_cpp;
 #endif
