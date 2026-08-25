39ecc120c54ee98c720b86f6b4f18b1cd41504f8
ShahVishrut <shahsvishrut@gmail.com>
2026-08-25 00:05:23 -0700

    Fixed

--- dynoram/src/dynamic/sonic_adapter.cc
+++ dynoram/src/dynamic/sonic_adapter.cc
@@ -726,10 +726,12 @@ double SonicORamAdapter::RawSonicBenchmark(int work_type, size_t batch_size) {
                 std::vector<uint8_t> in_buf(kSonicBlockBytes, 0);
                 std::vector<uint8_t> out_buf(kSonicBlockBytes, 0);
 
+                uint64_t num_leaves = impl_->client->topology().num_leaves;
+
                 for (size_t j = chunk_start + i; j < chunk_end; j += num_workers) {
                     uint64_t fake_address = j % capacity_; 
                     uint64_t fake_cur_leaf = impl_->pos_map[fake_address + 1];
-                    uint64_t fake_new_leaf = ((fake_address + 1) * 7331) % capacity_ + 1;
+                    uint64_t fake_new_leaf = ((fake_address + 1) * 7331) % num_leaves + 1;
 
                     if (fake_cur_leaf == UINT64_MAX) {
                         impl_->pos_map[fake_address + 1] = fake_new_leaf;
