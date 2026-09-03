8e7a22889785ce2e772c17e4a1776afe4a0d6d18
ShahVishrut <shahsvishrut@gmail.com>
2026-09-02 22:51:26 -0700

    Debug

--- dynoram/src/dynamic/oram.cc
+++ dynoram/src/dynamic/oram.cc
@@ -493,11 +493,15 @@ void ORam::ExecuteBatch(std::vector<BatchOperation>& batch, crypto::Key enc_key,
             std::cout << "  [DEBUG] Loop iter " << idx << " start, key=" << b.key_ << std::endl;
         }
         uint64_t phys_k = PhysicalKey(b.key_, 1);
+        std::cout << "    [DEBUG] phys_k computed" << std::endl;
         static_path_oram::Block new_b(0, phys_k);
-        // if (b.val_) {
-        //     new_b.val_ = std::move(b.val_);
-        // }
-        // sn_inserts.push_back(std::move(new_b));
+        std::cout << "    [DEBUG] new_b constructed" << std::endl;
+        if (b.val_) {
+            new_b.val_ = std::move(b.val_);
+        }
+        std::cout << "    [DEBUG] val moved" << std::endl;
+        sn_inserts.push_back(std::move(new_b));
+        std::cout << "    [DEBUG] pushed back" << std::endl;
         idx++;
     }
     std::cout << "[DEBUG] Pre-Phase 3: sn_inserts populated" << std::endl;
