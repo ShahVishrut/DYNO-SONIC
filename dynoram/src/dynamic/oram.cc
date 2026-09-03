9b594db7a51501f94de981b0a23a80d536a03de7
ShahVishrut <shahsvishrut@gmail.com>
2026-09-02 22:49:48 -0700

    Debug

--- dynoram/src/dynamic/oram.cc
+++ dynoram/src/dynamic/oram.cc
@@ -492,8 +492,8 @@ void ORam::ExecuteBatch(std::vector<BatchOperation>& batch, crypto::Key enc_key,
         if (idx % 100 == 0 || idx > 510) {
             std::cout << "  [DEBUG] Loop iter " << idx << " start, key=" << b.key_ << std::endl;
         }
-        // uint64_t phys_k = PhysicalKey(b.key_, 1);
-        // static_path_oram::Block new_b(0, phys_k);
+        uint64_t phys_k = PhysicalKey(b.key_, 1);
+        static_path_oram::Block new_b(0, phys_k);
         // if (b.val_) {
         //     new_b.val_ = std::move(b.val_);
         // }
