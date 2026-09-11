b2f467ec8612ae6230f79570af78b63d934f5068
ShahVishrut <shahsvishrut@gmail.com>
2026-09-11 00:20:54 -0700

    Fix

--- dynoram/src/dynamic/oram.cc
+++ dynoram/src/dynamic/oram.cc
@@ -450,7 +450,8 @@ void ORam::ExecuteBatch(std::vector<BatchOperation>& batch, crypto::Key enc_key,
       bool target_lt = static_cast<uint8_t>(a.is_target) < static_cast<uint8_t>(b.is_target);
       return sn::obliv::ct_select(target_lt, key_lt, key_eq);
   };
-  sn::sortshuffle::ser::bitonic::detail::bitonic_sort_impl(join_arr.data(), 2 * B, [](const JoinElement& e) { return e; }, comp_join1, sn::sortshuffle::ser::bitonic::detail::noop_hook{});
+  sn::sortshuffle::ser::bitonic::detail::noop_hook hook1;
+  sn::sortshuffle::ser::bitonic::detail::bitonic_sort_impl(join_arr.data(), 2 * B, [](const JoinElement& e) { return e; }, comp_join1, hook1);
 
   uint64_t cur_phys_k = 0;
   int8_t cur_sub_idx = -1;
@@ -471,7 +472,8 @@ void ORam::ExecuteBatch(std::vector<BatchOperation>& batch, crypto::Key enc_key,
       bool idx_lt = sn::obliv::ct_lt(a.batch_idx, b.batch_idx);
       return sn::obliv::ct_select(idx_lt, target_gt, target_eq);
   };
-  sn::sortshuffle::ser::bitonic::detail::bitonic_sort_impl(join_arr.data(), 2 * B, [](const JoinElement& e) { return e; }, comp_join2, sn::sortshuffle::ser::bitonic::detail::noop_hook{});
+  sn::sortshuffle::ser::bitonic::detail::noop_hook hook2;
+  sn::sortshuffle::ser::bitonic::detail::bitonic_sort_impl(join_arr.data(), 2 * B, [](const JoinElement& e) { return e; }, comp_join2, hook2);
 
   for (size_t i = 0; i < B; ++i) {
       batch[elems[i].seq].phys_k = join_arr[i].phys_k;
