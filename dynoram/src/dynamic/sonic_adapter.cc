e4cdddfb1980ef6c7be918e10c041933a33af06c
ShahVishrut <shahsvishrut@gmail.com>
2026-09-12 18:45:39 -0700

    Fix

--- dynoram/src/dynamic/sonic_adapter.cc
+++ dynoram/src/dynamic/sonic_adapter.cc
@@ -657,7 +657,7 @@ void SonicORamAdapter::InsertBatch(std::vector<static_path_oram::Block>& blocks,
               sn::oram::tree::block<kSonicBlockBytes> new_block{};
               // SONIC expects address = -1 for dummies
               new_block.address = sn::obliv::ct_select<uint64_t>(k - 1, static_cast<uint64_t>(-1), real);
-              new_block.leaf_ix = sn::obliv::ct_select<uint64_t>(leaf, static_cast<uint64_t>(-1), real);
+              new_block.leaf_ix = sn::obliv::ct_select<uint64_t>(leaf, 0, real);
               std::copy(in_buf.begin(), in_buf.end(), new_block.data.begin());
               
               impl_->client->insert(new_block);
